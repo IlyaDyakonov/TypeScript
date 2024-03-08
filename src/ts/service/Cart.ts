f6420591357b61b58d602fa28473751b324628e3
Илья АриоХ <123810575+IlyaDyakonov@users.noreply.github.com>
2024-03-08 23:26:40 +0400

    Update Cart.ts

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,5 +1,6 @@
 import Buyable from '../domain/Buyable';
 
+
 export default class Cart {
     private _items: Buyable[] = [];
 
@@ -13,26 +14,32 @@ export default class Cart {
 
     // считаем сумму price без учёта скидки
     sum(): number{
-        let amount = 0;
-        for (const item of this._items) {
-            amount += item.price;
-        }
-        return amount;
+        // let amount = 0;
+        // for (const item of this._items) {
+        //     amount += item.price;
+        // }
+        // return amount;
+        return this._items.reduce((sum, item) => sum + item.price,
+        0,);
     }
 
     // считаем сумму price с учётом скидки
     sumDiscount(sale: number): number{
         const amount = (this.sum() / 100) * sale;
-        const amountDiscount = this.sum() - amount;
-        return amountDiscount;
+        // const amountDiscount = this.sum() - amount;
+        return this.sum() - amount;
+        // return amountDiscount;
         }
 
     delete(id: number): void {
-        for (let i = 0; i < this._items.length; i++) {
-            if (this._items[i].id === id) {
-                this._items.splice(i, 1);
-                break;
-            }
-        }
+        // for (let i = 0; i < this._items.length; i++) {
+        //     if (this._items[i].id === id) {
+        //         this._items.splice(i, 1);
+        //         break;
+        //     }
+        // }
+        this._items = this._items.filter(function (n) {
+            return n.id !== id
+        })
     }
 }
